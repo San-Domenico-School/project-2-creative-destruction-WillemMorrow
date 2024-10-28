fde0d5ac3757fc3298d74f5e87c8241b2930aa8f
WillemMorrow <wmorrow27@sandomenico.org>
2024-10-28 15:06:23 -0700

    update 10.28.2024a

--- Assets/DB Scripts/DBDiskEnabler.cs
+++ Assets/DB Scripts/DBDiskEnabler.cs
@@ -2,26 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/***********************************
+ * script which disables the disk's meshrenderer component after it is instantiated
+ * 
+ * component of the disk prefab
+ * 
+ * 10.28.2024
+ * Pacifica Morrow
+ * ******************************/
+
 public class DBDiskEnabler : MonoBehaviour
 {
-    private MeshRenderer diskRenderer;
-    private float projectileVisualDelay = 0.1f;
+    private MeshRenderer diskRenderer;                //reference to the object's meshrenderer component
+    private float projectileVisualDelay = 0.1f;       //the time, in seconds before the projectile appears
 
+    //gets the meshrenderer component, sets it to false.
     private void Awake()
     {
         diskRenderer = GetComponent<MeshRenderer>();
         diskRenderer.enabled = false;
     }
 
-    // Start is called before the first frame update
+    // calls the Projectile Enabler function following Awake
     void Start()
     {
         StartCoroutine("ProjectileEnabler");
     }
 
+    //enables the meshrenderer after a delay
     IEnumerator ProjectileEnabler()
     {
-        Debug.Log("DiskEnablerWorking(?)");
+        //Debug.Log("DiskEnablerWorking(?)");
         yield return new WaitForSeconds(projectileVisualDelay);
         diskRenderer.enabled = true;
     }
